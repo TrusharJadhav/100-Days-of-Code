49337ee8cc3b65a32200e45a7ca203e81613afad
TrusharJadhav <56261588+TrusharJadhav@users.noreply.github.com>
2025-07-31 13:16:39 +0100

    Day 9

--- Day 8: Ceaser Cypher.py
+++ Day 8: Ceaser Cypher.py
@@ -1,18 +1,30 @@
 import string
-message=input("Type your Message: ").lower()
-scramble=int(input("Enter shift by how many letters: "))
 alphabets=[]
 str=string.ascii_lowercase
 for letter in str:
     alphabets.append(letter)
 
-Scrambled=alphabets[scramble:]+alphabets[:scramble]
-Scrambled_Message=""
+def encrypt(message,shift,direction):
+    Scrambled_Message=""
+    if direction=="encrypt":
+        shift=shift
+    elif direction=="decrypt":
+        shift=-1*shift
+    for letter in message:
+        if letter not in alphabets:
+            Scrambled_Message+=letter
+        else:
+            id=alphabets.index(letter)
+            delta=(id+shift)%26
+            Scrambled_Message+=alphabets[delta]
+    return Scrambled_Message
 
-for letter in message:
-    if letter==" ":
-        Scrambled_Message+=" "
-    else:
-        id=alphabets.index(letter)
-        Scrambled_Message+=Scrambled[id]
-print(Scrambled_Message)
+
+message=input("Type your Message: ").lower()
+shift=int(input("Enter shift by how many letters: "))
+direction=input("Do you want to Encrypt or Decrypt: ").lower()
+
+
+print(message)
+scrambled_message=encrypt(message,shift,direction)
+print(scrambled_message)
