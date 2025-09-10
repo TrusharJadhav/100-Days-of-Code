f4445c8217884d72a6c8cd73a6d6c40e6630a5ad
TrusharJadhav <56261588+TrusharJadhav@users.noreply.github.com>
2025-09-10 16:49:29 +0100

    Day 25

--- Day_25/Mail Merge Project Start/main.py
+++ Day_25/Mail Merge Project Start/main.py
@@ -1,13 +1,14 @@
+PLACEHOLDER="[name]"
 with open("./Input/Letters/starting_letter.txt","r") as content_draft:
-    draft=content_draft.readlines()
+    draft=content_draft.read()
 with open("./Input/Names/invited_names.txt","r") as content_names:
     invitees=content_names.readlines()
-print(invitees)
+
 for invitee in invitees:
-    draft[0]=f"Dear {invitee},"
-    text_block="\n".join(draft)
-    letter=f"letter for {invitee}.txt"
-    with open(f"./Output/{letter}","w") as invitation:
-        invitation.write(text_block)
+    stripped=invitee=invitee.strip()
+    invitation= draft.replace(PLACEHOLDER,invitee)
+    print(invitation)
+    with open(f"./Output/ReadyToSend/letter_for_ {invitee}.txt","w") as completed_letter:
+       completed_letter.write(invitation)
 
 
