53d4a61ae520e571cc3f031c01dcf17685190605
TrusharJadhav <56261588+TrusharJadhav@users.noreply.github.com>
2025-09-18 07:24:55 +0100

    Monday Motivational Quotes

---
+++ Day_32/Automated Quotes/main.py
@@ -0,0 +1,21 @@
+import smtplib
+import random
+import datetime as dt
+email="jadhavtrushar@gmail.com"
+password="emoe sobp gjij zrbz"
+data=[]
+with open("quotes.txt","r") as file:
+    data=file.readlines()
+    quote = random.choice(data)
+
+now=dt.datetime.now()
+if now.weekday()==0:
+    with smtplib.SMTP("smtp.gmail.com",port=587) as smtp:
+        smtp.starttls()
+        smtp.login(user=email,password=password)
+        subject="Motivational Quote"
+        body=quote
+        message=f"Subject:{subject}\n\n{body}"
+        smtp.sendmail(email,"drshilpakumbhar@gmail.com",message)
+
+    
